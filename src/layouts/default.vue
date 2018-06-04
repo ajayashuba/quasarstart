8fc642c3537234aad4afbe54a498b54061cb38e7
ajay.ashubaba <ajay.ashubaba@gmail.com>
2018-06-03 22:58:43 -0400

    d

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -75,7 +75,7 @@ export default {
   methods: {
     openURL
   },
-  mounted:function(){this.$nextTick(function(){console.log(this.$refs.abcd.name)})},
+  mounted:function(){this.$nextTick(function(){console.log(this)})},
   
 }
 </script>
